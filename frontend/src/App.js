e17dfc01297ebfa3a428e493bbf30298cbf78fa3
Raffael <rdavila754@gmail.com>
2025-04-26 23:35:17 -0400

    App js cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,11 @@
-import { useState } from "react";
 import ExpenseForm from "./components/ExpenseForm";
 
 function App() {
-  const [expenses, setExpenses] = useState([]);
-
-  const addExpense = (expense) => {
-    setExpenses([...expenses, expense]);
-  };
 
   return (
     <div>
       <h1>Budget Tracker</h1>
-      <ExpenseForm onAddExpense={addExpense} />
-      <ul>
-        {expenses.map((expense, index) => (
-          <li key={index}>
-            ${expense.amount.toFixed(2)} - {expense.description}
-          </li>
-        ))}
-      </ul>
+      <ExpenseForm/>
     </div>
 
   );
